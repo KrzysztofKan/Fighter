474d19fa9803bfc3893e541ec016a08187854c86
KrzysztofKan <krzysztof_kantoch@hotmail.com>
2023-05-04 17:16:22 +0200

    additional stats and function prototypes

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import colorist as cl
 
-# setup variables for balance
+# setup base variables for balance
 base_Hp = 10
 base_DMG = 1
 base_Strenght = 1
 base_Dexterity = 1
 base_Armor = 0
-
+# stats multipliers
 base_HP_per_Strenght = 5
 base_DMG_per_Strenght = 2
+base_SPD_per_Dexterity = 2
 
 
-# character class defining basik attributes and functions
+# character class defining basic attributes and functions
 class character():
     def __init__(self,player = False) -> None:
-        if player == False:
-            self.MAX_HP = base_Hp + (base_Strenght * base_HP_per_Strenght)
-            self.HP = self.MAX_HP
-            self.Attack_DMG = base_Strenght * base_DMG_per_Strenght
-        else:
-            self.MAX_HP = base_Hp + (base_Strenght * base_HP_per_Strenght)
-            self.HP = self.MAX_HP
-            self.Attack_DMG = base_Strenght * base_DMG_per_Strenght
+        self.LVL = 1
+        self.MAX_HP = base_Hp + (base_Strenght * base_HP_per_Strenght)
+        self.HP = self.MAX_HP
+        self.Attack_DMG = base_Strenght * base_DMG_per_Strenght
+        self.SPD = base_Dexterity
+
 
     
 
@@ -32,6 +31,8 @@ class character():
     def heal_HP():
         pass
     def print_character():
+        pass
+    def get_SPD():
         pass
     def isalive():
         pass
