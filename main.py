4327745667b69df1aa2b654d7228c8cdc641370b
KrzysztofKan <krzysztof_kantoch@hotmail.com>
2023-05-04 16:48:53 +0200

    Prototyped all basic character functions

--- main.py
+++ main.py
@@ -4,19 +4,37 @@ import colorist as cl
 base_Hp = 10
 base_DMG = 1
 base_Strenght = 1
+base_Dexterity = 1
 base_Armor = 0
 
 base_HP_per_Strenght = 5
-
-
-
+base_DMG_per_Strenght = 2
 
 
 # character class defining basik attributes and functions
 class character():
-    def __init__(self) -> None:
-        pass
+    def __init__(self,player = False) -> None:
+        if player == False:
+            self.MAX_HP = base_Hp + (base_Strenght * base_HP_per_Strenght)
+            self.HP = self.MAX_HP
+            self.Attack_DMG = base_Strenght * base_DMG_per_Strenght
+        else:
+            self.MAX_HP = base_Hp + (base_Strenght * base_HP_per_Strenght)
+            self.HP = self.MAX_HP
+            self.Attack_DMG = base_Strenght * base_DMG_per_Strenght
 
+    
+
+    def receive_DMG():
+        pass
+    def deal_DMG():
+        pass
+    def heal_HP():
+        pass
+    def print_character():
+        pass
+    def isalive():
+        pass
 
 
 
@@ -27,3 +45,6 @@ class character():
 # main loop/logic
 def main():
     return
+
+
+main()
