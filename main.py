421e29574252eb3142eee1495b67edc7effd1519
KrzysztofKan <krzysztof_kantoch@hotmail.com>
2023-05-04 16:30:55 +0200

    Base stats layout

---
+++ main.py
@@ -0,0 +1,29 @@
+import colorist as cl
+
+# setup variables for balance
+base_Hp = 10
+base_DMG = 1
+base_Strenght = 1
+base_Armor = 0
+
+base_HP_per_Strenght = 5
+
+
+
+
+
+# character class defining basik attributes and functions
+class character():
+    def __init__(self) -> None:
+        pass
+
+
+
+
+
+
+
+
+# main loop/logic
+def main():
+    return
