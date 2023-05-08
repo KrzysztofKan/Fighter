daae3f73b5b217252abcfda58aa4116a55a5f077
KrzysztofKan <krzysztof_kantoch@hotmail.com>
2023-05-08 14:17:26 +0200

    Aditional stats added

--- main.py
+++ main.py
@@ -3,28 +3,33 @@ import colorist as cl
 # setup base variables for balance
 base_Hp = 10
 base_DMG = 1
-base_Strenght = 1
-base_Dexterity = 1
+base_Strenght = 5
+base_Dexterity = 5
+base_Inteligence = 5
 base_Armor = 0
 # stats multipliers
-base_HP_per_Strenght = 5
+str_HP_Mult = 22
+str_HPreg_Mult = 0.1
+agi_SPD_Mult = 1
+agi_Armor_Mult = 0.167
+int_Mana_Mult = 12
+int_ManaReg_Mult = 0.05
+int_MagRes_Mult = 0.1
+
 base_DMG_per_Strenght = 2
-base_SPD_per_Dexterity = 2
 
 
 # character class defining basic attributes and functions
 class character():
-    def __init__(self,player = False) -> None:
-        self.LVL = 1
-        self.name = 'Joe'
-        self.MAX_HP = base_Hp + (base_Strenght * base_HP_per_Strenght)
+    def __init__(self,player = False,LVL = 1,name = "") -> None:
+        self.LVL = LVL
+        self.name = name
+        self.MAX_HP = base_Hp + (base_Strenght * str_HP_Mult)
         self.HP = self.MAX_HP
         self.Attack_DMG = base_Strenght * base_DMG_per_Strenght
-        self.SPD = base_Dexterity
+        self.SPD = base_Dexterity * agi_SPD_Mult
+        self.Armor = base_Armor + (base_Dexterity * agi_Armor_Mult)
         self.alive = True
-
-
-
 
     def receive_DMG(self,damage):
         self.HP -= damage
