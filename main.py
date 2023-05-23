8a5dd4d44498320857a8a881742638a606a1213a
KrzysztofKan <krzysztof_kantoch@hotmail.com>
2023-05-23 10:46:21 +0200

    Changing gui to pyimgui

--- main.py
+++ main.py
@@ -1,89 +1,116 @@
+import colorsys
+import imgui
+import os
+import sys
 import character as ch
 import tkinter as tk
 import tkinter.ttk as ttk
 from PIL import ImageTk,Image
 
+
 # main loop/logic
 def main():
 
-    pl = ch.character(True,1,"John")
-    en = ch.character(False,1,"Wolf")
+    # initilize imgui context (see documentation)
 
-    def lvlup():
-        pl.level_up()
-        i = pl.get_level()
-        player_lvl2["text"] = f"{i}"
-        player_XPBar.step(99)
+    imgui.create_context()
+    imgui.get_io().display_size = 100, 100
+    imgui.get_io().fonts.get_tex_data_as_rgba32()
+        # start new frame context
+    imgui.new_frame()
+
+        # open new window context
+    imgui.begin("Your first window!", True)
+
+        # draw text label inside of current window
+    imgui.text("Hello world!")
+
+        # close current window context
+    imgui.end()
+
+        # pass all drawing comands to the rendering pipeline
+        # and close frame context
+    imgui.render()
+    imgui.end_frame()
+
+    # pl = ch.character(True,1,"John")
+    # en = ch.character(False,1,"Wolf")
+
+    # def lvlup():
+    #     pl.level_up()
+    #     i = pl.get_level()
+    #     player_lvl2["text"] = f"{i}"
+    #     player_XPBar.step(99)
     
-    def getXP():
-        player_XPBar.step(10)
+    # def getXP():
+    #     player_XPBar.step(10)
 
-    #Window setup
-    window = tk.Tk(className="Fighter")
-    window.columnconfigure([0,1,2],minsize=250,uniform=True)
-    window.resizable(False,False)
+    # #Window setup
+    # window = tk.Tk(className="Fighter")
+    # window.columnconfigure([0,1,2],minsize=250,uniform=True)
+    # window.resizable(False,False)
 
-    #Player Card setup
-    player_frame = tk.Frame(master=window,relief="raised",width=200,height=100)
-    player_frame.grid(sticky="nsew",row=0,column=0)
+    # #Player Card setup
+    # player_frame = tk.Frame(master=window,relief="raised",width=200,height=100)
+    # player_frame.grid(sticky="nsew",row=0,column=0)
 
-    #Player portrait
-    #portrait = ImageTk.PhotoImage(Image.open("C:\Python\Fighter\Assets\Portrait_Placeholder.bmp"))
-    image = Image.open("C:\Python\Fighter\Assets\Portrait_Placeholder.bmp")
-    image = image.resize((100,100))
-    portrait = ImageTk.PhotoImage(image)
+    # #Player portrait
+    # #portrait = ImageTk.PhotoImage(Image.open("C:\Python\Fighter\Assets\Portrait_Placeholder.bmp"))
+    # image = Image.open("C:\Python\Fighter\Assets\Portrait_Placeholder.bmp")
+    # image = image.resize((100,100))
+    # portrait = ImageTk.PhotoImage(image)
     
-    portrait_frame = tk.Frame(master=player_frame,width=1,height=1,relief="raised")
-    player_portrait = tk.Label(master=portrait_frame,image=portrait,anchor="center",width=100,height=100,justify="right")
-    player_portrait.grid(row=0,column=0)
-    portrait_frame.grid(row=0,column=4,columnspan=2,rowspan=10)
-    
-
-    #player name
-    player_namel = tk.Label(master=player_frame,text="Name: ")
-    player_namel.grid(row=0,column=0)
-    player_name = tk.Label(master=player_frame,text=pl.get_name())
-    player_name.grid(row=0,column=1)
-    #player LVL
-    player_lvl1 = tk.Label(master=player_frame,text="LVL: ",justify="left")
-    player_lvl1.grid(row=1,column=0)
-    player_lvl2 = tk.Label(master=player_frame,text=pl.get_level(),justify="center")
-    player_lvl2.grid(row=1,column=1)
-    player_lvl3 = tk.Button(master=player_frame,text="LVL Up",command=lvlup,overrelief="ridge")
-    player_lvl3.grid(row=1,column=2)
-    #playerEXP
-    player_XPBar = ttk.Progressbar(master=player_frame,maximum=100,mode="determinate")
-    player_XPBar.grid(row=3,column=1,columnspan=3)
-    player_XPBar
-    player_XPBar.grid(row=3,column=1,columnspan=3)
-    player_XP = tk.Label(master=player_frame,text="XP")
-    player_XP.grid(row=3,column=0)
-    player_EXP = tk.Label(master=player_frame,text="0")
-    player_EXP.grid(row=3,column=1)
-    player_EXPsep= tk.Label(master=player_frame,text="/")
-    player_EXPsep.grid(row=3,column=2)
-    player_MEXP = tk.Label(master=player_frame,text="100",justify="left")
-    player_MEXP.grid(row=3,column=3)
+    # portrait_frame = tk.Frame(master=player_frame,width=1,height=1,relief="raised")
+    # player_portrait = tk.Label(master=portrait_frame,image=portrait,anchor="center",width=100,height=100,justify="right")
+    # player_portrait.grid(row=0,column=0)
+    # portrait_frame.grid(row=0,column=4,columnspan=2,rowspan=10)
     
 
-    #Actions panel Setup
-    mid_frame = tk.Frame(master=window,relief="flat",width=100,background="black")
-    mid_frame.grid(sticky="nsew",row=0,column=1)
+    # #player name
+    # player_namel = tk.Label(master=player_frame,text="Name: ")
+    # player_namel.grid(row=0,column=0)
+    # player_name = tk.Label(master=player_frame,text=pl.get_name())
+    # player_name.grid(row=0,column=1)
+    # #player LVL
+    # player_lvl1 = tk.Label(master=player_frame,text="LVL: ",justify="left")
+    # player_lvl1.grid(row=1,column=0)
+    # player_lvl2 = tk.Label(master=player_frame,text=pl.get_level(),justify="center")
+    # player_lvl2.grid(row=1,column=1)
+    # player_lvl3 = tk.Button(master=player_frame,text="LVL Up",command=lvlup,overrelief="ridge")
+    # player_lvl3.grid(row=1,column=2)
+    # #playerEXP
+    # player_XPBar = ttk.Progressbar(master=player_frame,maximum=100,mode="determinate")
+    # player_XPBar.grid(row=3,column=1,columnspan=3)
+    # player_XPBar
+    # player_XPBar.grid(row=3,column=1,columnspan=3)
+    # player_XP = tk.Label(master=player_frame,text="XP")
+    # player_XP.grid(row=3,column=0)
+    # player_EXP = tk.Label(master=player_frame,text="0")
+    # player_EXP.grid(row=3,column=1)
+    # player_EXPsep= tk.Label(master=player_frame,text="/")
+    # player_EXPsep.grid(row=3,column=2)
+    # player_MEXP = tk.Label(master=player_frame,text="100",justify="left")
+    # player_MEXP.grid(row=3,column=3)
+    
 
-    #Enemy Card Setup
-    enemy_frame = tk.Frame(master=window,relief="raised",width=200)
-    enemy_frame.grid(sticky="nsew",row=0,column=2)
+    # #Actions panel Setup
+    # mid_frame = tk.Frame(master=window,relief="flat",width=100,background="black")
+    # mid_frame.grid(sticky="nsew",row=0,column=1)
 
-    #player name
-    enemy_namel = tk.Label(master=enemy_frame,text="Name: ")
-    enemy_namel.grid(row=0,column=0)
-    enemy_name = tk.Label(master=enemy_frame,text=en.get_name())
-    enemy_name.grid(row=0,column=1)
+    # #Enemy Card Setup
+    # enemy_frame = tk.Frame(master=window,relief="raised",width=200)
+    # enemy_frame.grid(sticky="nsew",row=0,column=2)
 
-    ver = tk.Label(master=window,text="Unversioned-Alpha",justify="right")
-    ver.grid(row=1,column=2)
+    # #player name
+    # enemy_namel = tk.Label(master=enemy_frame,text="Name: ")
+    # enemy_namel.grid(row=0,column=0)
+    # enemy_name = tk.Label(master=enemy_frame,text=en.get_name())
+    # enemy_name.grid(row=0,column=1)
 
-    window.mainloop()
+    # ver = tk.Label(master=window,text="Unversioned-Alpha",justify="right")
+    # ver.grid(row=1,column=2)
+
+    # window.mainloop()
    
 
 
